59cb66e79331f5fb79134ef0e1eb62fcb027695c
Neha Daga <daga@mac-zaugg05.fritz.box>
2024-04-08 21:00:40 +0200

    Modified and added new functions

--- R/Used_fn.R
+++ R/Used_fn.R
@@ -188,3 +188,374 @@ to_get_bed_df_peaks_from_peak_id_vector = function(input_peak_id_vector,bed_file
 
   return(peak_id_df)
 }
+
+
+################### PCA, heatmap,correlogram, ggpairs
+#' Computing PCA
+#'
+#' @param data_mat Count matrix rld or vst transformed and normalised
+#' @param ntop Top n variable features
+#' @param meta_df Data frame of covariates
+#'
+#' @return Data frame with PCA loadings and covariates
+#' @export
+#'
+#' @examples
+
+
+
+compute_pca <- function(data_mat, ntop,meta_df){
+
+  Pvars <- rowVars(data_mat)
+  select <- order(Pvars, decreasing = TRUE)[seq_len(min(ntop,
+                                                        length(Pvars)))]
+
+  PCA <- prcomp(t(data_mat[select, ]), scale. = F, center = TRUE)
+  percentVar <- round(100*PCA$sdev^2/sum(PCA$sdev^2),1)
+
+  pca_df = data.frame(PC1 = PCA$x[,1], PC2 = PCA$x[,2],
+                      PC3 = PCA$x[,3], PC4 = PCA$x[,4],PC5 = PCA$x[,5],PC6 = PCA$x[,6])
+
+  #meta_df = as.data.frame(colData(data_mat))
+
+  pca_meta_df = cbind(pca_df, meta_df)
+
+  return(list(PCA_meta_df = pca_meta_df,
+              perc_var = percentVar,
+              selected_vars = select))
+
+}
+
+
+######## Correlogram
+#' Correlogram of pca loadings and covariates
+#'
+#' @param count_matrix Count matrix rld or vst transformed normalised
+#' @param ntop top n features
+#' @param metadata Covariates data frame
+#' @param covs Column names of covariates
+#' @param scale logical value TRUE or FALSE
+#' @param pcCount Number of dimension
+#' @param outdir Output file name for correlogram
+#' @param logTransform Deafault is TRUE
+#'
+#' @return Heatmap of correlogram between PCA loadings and covariates
+#' @export
+#'
+#' @examples
+
+pca.correlogram <- function(count_matrix,ntop, metadata, covs, scale, pcCount, outdir, logTransform = TRUE){
+
+
+  Pvars <- rowVars(count_matrix)
+  select <- order(Pvars, decreasing = TRUE)[seq_len(min(ntop,
+                                                        length(Pvars)))]
+
+
+
+  ## adj. R2-values ##
+
+  r2_fun <- function(x, y) {
+    summary(lm(y ~ x))$adj.r.squared
+  } #function to get adj. R2 values
+
+  PCA <- prcomp(t(count_matrix[select, ]), scale = scale)
+  pcs_cv <- PCA$x[,1:pcCount]   #Top PCs from top 1000 variable genes
+  eig.val <- 100*summary(PCA)$importance[2,1:pcCount]
+  labels <- paste0(colnames(pcs_cv), "_",  "(", round(eig.val, 2),  " %)")
+
+  ###### Metadata
+
+  covariates <- data.frame(metadata[,covs],
+                           row.names=colnames(count_matrix))    #Clinical covariates
+
+  r2_cv <- matrix(NA, nrow = ncol(covariates), ncol = ncol(pcs_cv),
+                  dimnames = list(colnames(covariates), colnames(pcs_cv)))
+
+  for (cov in colnames(covariates)) {
+    for (pc in colnames(pcs_cv)) {
+      r2_cv[cov, pc] <- r2_fun(covariates[,cov], pcs_cv[,pc])
+    }
+  }      #get adj. R2 value for covariates ~ PC 1-6
+
+  r2CvPlot <- pheatmap::pheatmap(r2_cv, cluster_cols = F, display_numbers = T,labels_col = labels,
+                       color = colorRampPalette((RColorBrewer::brewer.pal(n = 9, name = "Reds")))(1000))
+
+  xlabCur = "Mean log counts"
+
+  if (logTransform == TRUE) {
+    densPlot <- function() {
+      dens <- apply(log2(count_matrix+1), 2, function(x) density(x, na.rm = TRUE))
+      plot(NA,xlim=range(sapply(dens, "[", "x")), ylim=range(sapply(dens, "[", "y")))
+      mapply(lines, dens, col=1:length(dens))
+    }
+  }
+  else {
+    densPlot <- function() {
+      dens <- apply(count_matrix, 2, density)
+
+      plot(NA, xlim=range(sapply(dens, "[", "x")), ylim=range(sapply(dens, "[", "y")))
+      mapply(lines, dens, col=1:length(dens))
+
+    }
+  }
+
+  pdf(outdir, height=10, width=10)
+
+  grid::grid.draw(r2CvPlot$gtable)
+  densPlot()
+
+  dev.off()
+
+}
+
+
+#' Clustered heatmap based on variable features
+#'
+#' @param data_mat Count matrix (rld or vst transformed, normalised)
+#' @param ntop Top n variable features
+#' @param col_select Vector of annotation column names
+#' @param meta_df Row annotation data frame
+#' @param outfn Output filename
+#' @param rownorm Z-score across rows. Logical value TRUE/FALSE
+#'
+#' @return Heatmap based on n variable features
+#' @export
+#'
+#' @examples
+
+draw_heatmap_top_peaks = function (data_mat,ntop,col_select,meta_df,outfn,rownorm) {
+
+  Pvars <- rowVars(data_mat)
+  select <- order(Pvars, decreasing = TRUE)[seq_len(min(ntop,
+                                                        length(Pvars)))]
+
+  ##### DataFrame
+  input_df = as.data.frame(data_mat)
+  input_df$rowvar = Pvars
+  input_df = input_df[order(input_df$rowvar,decreasing = TRUE),]
+
+  ####### Top peak mat
+  data_mat_sub = data_mat[select, ]
+
+
+  ######### Normalises across rowa
+  cal_z_score <- function(x){
+    (x - mean(x,na.rm = TRUE)) / sd(x,na.rm = TRUE)
+  }
+
+  data_mat_norm <- t(apply(data_mat_sub, 1, cal_z_score))
+
+
+  ############# To get input mat and if row normalised or not
+  if (rownorm == TRUE) {
+    input_mat = data_mat_norm
+
+  }else{
+    input_mat = data_mat_sub
+  }
+
+
+  ##### For annotation
+  #meta_df = as.data.frame(colData(data_mat))
+  annotate_col_df = meta_df[,col_select]
+  rownames(annotate_col_df) = rownames(meta_df)
+
+  annotate_row_df = data.frame("Peak_var" = input_df$rowvar[1:ntop])
+  rownames(annotate_row_df) = rownames(input_df)[1:ntop]
+
+  ########Check
+  check1 = all(rownames(annotate_row_df) == rownames(input_mat))
+  check2 = all(colnames(input_mat) == rownames(annotate_col_df))
+
+
+  if (check1 & check2) {
+
+    p1 = pheatmap::pheatmap(input_mat,annotation_col = annotate_col_df,cluster_rows = T,cluster_cols = T,show_rownames = F,annotation_row = annotate_row_df)
+    p2 = pheatmap::pheatmap(input_mat,annotation_col = annotate_col_df,cluster_rows = F,cluster_cols = F,show_rownames = F,annotation_row = annotate_row_df)
+
+    pdf(file = outfn,width = 10,height = 10,onefile = TRUE,useDingbats = FALSE)
+
+    grid::grid.newpage()
+    grid::grid.draw(p1$gtable)
+
+    grid::grid.newpage()
+    grid::grid.draw(p2$gtable)
+
+    dev.off()
+
+  }
+
+}
+
+
+
+##### Diagniostic plots related to differential expression (MA, volcano plot)
+
+#' MA Plot labelled with significant genes
+#'
+#' @param res DESeq2 result table of differential analysis
+#' @param thresh Adjusted p-value threshold. Default is 0.05
+#' @param labelsig Label genes below threshold. Logical value TRUE or FALSE. Default is TRUE
+#' @param textcx Text size. Default is 1
+#' @param ...Other. Parameters for base R plot function
+#'
+#' @return MA plot for differential analysis
+#' @export
+#'
+#' @examples
+
+
+maplot <- function (res, thresh=0.05, labelsig=FALSE, textcx=1, ...) {
+
+  res = as.data.frame(res)
+  with(res, plot(baseMean, log2FoldChange, pch=20, cex=.5, log="x", ...))
+  with(subset(res, padj<thresh), points(baseMean, log2FoldChange, col="red", pch=20, cex=1.5))
+
+  if (labelsig) {
+    require(calibrate)
+    Gene = rownames(subset(res, padj<thresh))
+    base::with(subset(res, padj<thresh), textxy(baseMean, log2FoldChange, labs=Gene, cex=textcx, col=2))
+  }
+}
+
+
+
+## Volcano plot with "significant" genes labeled
+
+#' Volcano plot with "significant" genes labeled
+#'
+#' @param res DESeq2 result table of differential analysis
+#' @param lfcthresh Log2 Fold change threshold. Default is 2
+#' @param sigthresh Significant p-value threshold. Default is 0.05
+#' @param main plot title
+#' @param legendpos Position of legend . The value can be c()
+#' @param labelsig Label genes below sigthresh and above lfcthresh. Logical value TRUE or FALSE. Default is TRUE
+#' @param textcx text size. Default is 1
+#' @param ...Parameters other parameters for base R plot function
+#'
+#' @return Volcano plot with labelled genes
+#'
+#' @export
+#'
+#' @examples
+
+volcanoplot <- function (res, lfcthresh=2, sigthresh=0.05, main="Volcano Plot", legendpos="topright", labelsig=FALSE, textcx=1, ...) {
+
+  res = as.data.frame(res)
+  with(res, plot(log2FoldChange, -log10(pvalue), pch=20, main=main, ...))
+  with(subset(res, padj<sigthresh ), points(log2FoldChange, -log10(pvalue), pch=20, col="red", ...))
+  with(subset(res, abs(log2FoldChange)>lfcthresh), points(log2FoldChange, -log10(pvalue), pch=20, col="orange", ...))
+  with(subset(res, padj<sigthresh & abs(log2FoldChange)>lfcthresh), points(log2FoldChange, -log10(pvalue), pch=20, col="green", ...))
+  if (labelsig) {
+    require(calibrate)
+    Gene = rownames(subset(res, padj<sigthresh & abs(log2FoldChange)>lfcthresh))
+    with(subset(res, padj<sigthresh & abs(log2FoldChange)>lfcthresh), textxy(log2FoldChange, -log10(pvalue), labs=Gene, cex=textcx, ...))
+  }
+  legend(legendpos, xjust=1, yjust=1, legend=c(paste("FDR<",sigthresh,sep=""), paste("|LogFC|>",lfcthresh,sep=""), "both"), pch=20, col=c("red","orange","green"))
+}
+
+
+
+
+##### Mapping gene ids
+#' Mapping ENSEMBL to gene symbols
+#'
+#' @param input_ensembl_vector Vector of ensembl ids to be mapped
+#'
+#' @return Data frame with gene symbols, entrezid
+#'
+#' @export
+#'
+#' @examples
+
+to_add_gene_symbol_ENSEMBL = function (input_ensembl_vector) {
+
+  ensembl <- biomaRt::useEnsembl(biomart = "ensembl", dataset = "hsapiens_gene_ensembl")
+
+  gene_mapping_df = biomaRt::getBM(mart = ensembl,attributes = c("entrezgene_id","ensembl_gene_id","hgnc_symbol"),
+                          filters = "ensembl_gene_id",values = input_ensembl_vector)
+  return (gene_mapping_df)
+}
+
+
+
+###### Enrichment analysis
+#' Title
+#'
+#' @param expressedTarget Forground genes (ENTREZ IDs)
+#' @param expressedGenes Background/universe genes (ENTREZ IDs)
+#' @param pval_thresh Significant p-val threshold. Default is 0.05
+#' @param ont Default is biological process (BP). Value can be any/all GO term "BP", "MF" or "CC
+#'
+#' @return List of GO and kegg enrichment
+#'
+#' @export
+#'
+#' @examples
+
+
+to_get_GO_kegg_enrichment = function (expressedTarget,expressedGenes,pval_thresh = 0.05,ont = "BP") {
+
+
+  GO <- clusterProfiler::enrichGO(gene = expressedTarget,
+                 OrgDb='org.Hs.eg.db',
+                 pvalueCutoff= 1,
+                 ont = ont,
+                 keyType= "ENTREZID",
+                 universe=expressedGenes)
+
+  GO_df= GO@result
+  GO_df = GO_df[order(GO_df$p.adjust),]
+
+  kegg <- clusterProfiler::enrichKEGG(gene = expressedTarget,
+                   organism  = "human",
+                   pvalueCutoff = 0.05,universe = expressedGenes)
+
+  kegg_df =  kegg@result
+  kegg_df =  kegg_df[order(kegg_df$p.adjust),]
+
+
+  return (list(GO_df,kegg_df))
+
+}
+
+
+#' Fisher exact test
+#'
+#' @param enrichment_category_groups Functional enrichment group name vector (colnames)
+#' @param input_category_groups_to_be_tested Input category group name vector (rownames)
+#' @param x overlap of hit list with category of interest
+#' @param n total no of genes (universe)
+#' @param m  number of genes in a category of interest
+#' @param k length of hit list
+#' @param ... other parameters to be passed in fisher.test function
+#'
+#' @return List of contingency table and result of fisher test
+#'
+#' @export
+#'
+#' @examples
+
+to_do_fisher_exact_test = function (enrichment_category_groups,input_category_groups_to_be_tested,x,n,m,k,...) {
+
+  #k = length of hit list;
+  #x = overlap of hit list with category of interest;
+  #m = number of genes in a category of interest;
+  #n = total no of genes (universe)
+
+  data_vector = c(x,k-x,m-x,((n-m)-(k-x)) )
+
+  contingency.table = matrix(data_vector,nrow = 2,ncol = 2,byrow = TRUE)
+  colnames(contingency.table) <- enrichment_category_groups
+  rownames(contingency.table) <- input_category_groups_to_be_tested
+
+
+  f.test = fisher.test(contingency.table,...)
+
+  return (list(f.test,contingency.table))
+
+}
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-R package for commonnly used R functions from GenomicRanges
+
+# Usefn
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+This package contains commonly used R functions which I have used for the analysis and visualisation of genomic/transcriptomic data
+
+## Installation
+
+You can install the development version of Usefn from [GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("nd-nehadaga/Usefn")
+```
+
+## Example
+
+``` r
+library(Usefn)
+## To find overlap between two vectors of genomic coordinates
+
+enhancer1 = c("ch2:1002-1500","ch1:30000-38000","ch5:70000-78252")
+enhancer2 = c("ch2:1100-1200","ch1:37655-38000" ,"ch4:50000-57122")
+
+```
+
